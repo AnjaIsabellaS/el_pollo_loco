0c3ac02dcee7530a66608e5c5eb6715b83c52cc2
Anja-Isabella Schulz <anja-isabella.schulz@gmx.de>
2026-06-30 10:31:13 +0200

    feat: character-following camera added

--- models/character.class.js
+++ models/character.class.js
@@ -32,8 +32,8 @@ class Character extends MovableObject {
             if (this.world.keyboard.LEFT) {
                 this.x -= this.speed;
                 this.otherDirection = true;
-
             }
+            this.world.camera_x = -this.x;
         }, 1000 / 60);
 
 
