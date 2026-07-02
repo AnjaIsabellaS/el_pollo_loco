1d85f1a5c88a6bbac21fdbad0c5e2cea879990c5
Anja-Isabella Schulz <anja-isabella.schulz@gmx.de>
2026-07-02 08:33:09 +0200

    refactor: collision detection

--- models/world.class.js
+++ models/world.class.js
@@ -55,7 +55,7 @@ class World {
         this.ctx.beginPath();
         this.ctx.lineWidth = '5';
         this.ctx.strokeStyle = 'blue';
-        this.ctx.rect(mo.x, mo.y, mo.x + mo.width, mo.y + mo.height);
+        this.ctx.rect(mo.img, mo.x, mo.y, mo.width, mo.height);
         this.ctx.stroke();
 
         if (mo.otherDirection) {
